9fcd3e59dd9f469062601a2ffc097f0ce395ac24
Sam Coco <Samuel.HenriAC@outlook.com>
2019-11-14 15:04:19 +0000

    Relevant information extracted from JSON Objects and displayed

--- src/api/weather/Weather.java
+++ src/api/weather/Weather.java
@@ -1,12 +1,14 @@
 package api.weather;
 
 import java.util.Scanner;
+import org.json.JSONObject;
 
 public class Weather implements Runnable {
 
     private static final String URI = "https://api.openweathermap.org/data/2.5/weather";
     private static final String API_KEY = "c38b28e71f71ffadffd8a9125ee2871a";
     private static final String COUNTRY = "uk";
+    private static final String UNITS = "Metric";
     private Connection connection;
 
 
@@ -16,26 +18,45 @@ public class Weather implements Runnable {
     }
 
     private void printWeatherData(String responseData){
-        System.out.println(responseData);
+        if(responseData != null){
+            JSONObject data = new JSONObject(responseData);
+
+            int temperature = data.getJSONObject("main").getInt("temp");
+            int windSpeed = data.getJSONObject("wind").getInt("speed");
+            String description = data.getJSONArray("weather").getJSONObject(0).getString("description");
+
+            System.out.println(
+                "Country: " + COUNTRY.toUpperCase() + "\n" +
+                "Temperature: " + temperature + "°C" + "\n" +
+                "Summary: " + description + "\n" +
+                "Wind: " + windSpeed + " mph"
+            );
+        }
     }
 
     public void run(){
     // Example - https://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=c38b28e71f71ffadffd8a9125ee2871a
-        connection = new Connection(URI, API_KEY);
+        connection = new Connection();
         Scanner scanner = new Scanner(System.in);
         while(true){
-            System.out.println("************************************************************************");
-            System.out.println("Enter UK city name for a weather report: ");
+            System.out.println("----------------------------------------------------------------------------------------");
+            System.out.println("Enter " + COUNTRY.toUpperCase() + " city name for weather report: ");
             String input = scanner.next();
 
             if(input.equals("quit")){
                 connection.getConnection().disconnect();
+                System.out.println("APPLICATION TERMINATED");
                 return;
             }
 
-            String queryString = "?q=" + input + "," + COUNTRY;
+            String queryString = URI + "?q=" + input + "," + COUNTRY + "&units=" + UNITS + "&appid=" + API_KEY;
             String response = connection.query(queryString);
-            printWeatherData(response);
+
+            if(response != null){
+                printWeatherData(response);
+            } else {
+                System.out.println("No Server Response Received");
+            }
         }
     }
 }
