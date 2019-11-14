fd0d8af8a3a4cbf987665a40fd34b92d2ae4ef7e
Sam Coco <Samuel.HenriAC@outlook.com>
2019-11-14 09:02:30 +0000

    Main moved to 'weather' package

---
+++ src/api/weather/Main.java
@@ -0,0 +1,11 @@
+package api.weather;
+
+import api.weather.Weather;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Weather weather = new Weather();
+        weather.query();
+    }
+}
