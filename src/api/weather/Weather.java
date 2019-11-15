c77a48fac159b4329565223c9de66811ee677b0d
Sam Coco <Samuel.HenriAC@outlook.com>
2019-11-15 23:27:41 +0000

    Documentation added

--- src/api/weather/Weather.java
+++ src/api/weather/Weather.java
@@ -11,12 +11,20 @@ public class Weather implements Runnable {
     private static final String UNITS = "Metric";
     private Connection connection;
 
-
+    /**
+     * Calls the thread run method for an instantiated weather object,
+     * to allow user to perform weather queries, based on locations, with the "Openweathermap API".
+     */
     public void query(){
         Thread thread = new Thread(this);
         thread.start();
     }
 
+    /**
+     * Prints the JSON response data from 'Openweathermap API', specifically the following
+     * data: country, temperature, description, and wind speed.
+     * @param responseData The JSON response, in string form, from a 'Openweathermap API' GET request.
+     */
     private void printWeatherData(String responseData){
         if(responseData != null){
             JSONObject data = new JSONObject(responseData);
@@ -34,6 +42,11 @@ public class Weather implements Runnable {
         }
     }
 
+    /**
+     * Starts new thread, which handles prompting user for input for location of weather a query,
+     * forming the URL to perform an 'Openweathermap API' GET request, and terminating the
+     * application.
+     */
     public void run(){
     // Example - https://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=c38b28e71f71ffadffd8a9125ee2871a
         connection = new Connection();
